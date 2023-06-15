5612195de5e6bae422ff190c5de50959385cedff
gaurav-singh-panwar <55618776+gaurav-singh-panwar@users.noreply.github.com>
2023-06-15 20:33:43 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
 //your code here
+cy.get('audio').should(($audio) => {
+  const urls = [
+    'http://commondatastorage.googleapis.com/codeskulptor-demos/DDR_assets/Kangaroo_MusiQue_-_The_Neverwritten_Role_Playing_Game.mp3',
+    'http://commondatastorage.googleapis.com/codeskulptor-assets/Epoq-Lepidoptera.ogg',
+    'http://codeskulptor-demos.commondatastorage.googleapis.com/pang/paza-moduless.mp3'
+  ];
+  const audioUrls = $audio.toArray().map((audio) => audio.currentSrc);
+  expect(audioUrls).to.deep.equal(urls);
+});
