6dd668b4ecb5c21b89aa2cd4937255cd677fa6f9
gaurav-singh-panwar <55618776+gaurav-singh-panwar@users.noreply.github.com>
2023-06-15 20:39:40 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,10 +1,5 @@
-//your code here
-cy.get('audio').should(($audio) => {
-  const urls = [
-    'http://commondatastorage.googleapis.com/codeskulptor-demos/DDR_assets/Kangaroo_MusiQue_-_The_Neverwritten_Role_Playing_Game.mp3',
-    'http://commondatastorage.googleapis.com/codeskulptor-assets/Epoq-Lepidoptera.ogg',
-    'http://codeskulptor-demos.commondatastorage.googleapis.com/pang/paza-moduless.mp3'
-  ];
-  const audioUrls = $audio.toArray().map((audio) => audio.currentSrc);
-  expect(audioUrls).to.deep.equal(urls);
+describe('example to-do app', () => {
+  it('3 audios with heading are there', () => {
+    // your test code here
+  }, { timeout: 10000 }); // Increase timeout to 10000ms (10 seconds)
 });
