51b59fe9bc2dc3da00f34ef9b18f47db2dc2e17d
gaurav-singh-panwar <55618776+gaurav-singh-panwar@users.noreply.github.com>
2023-06-15 20:43:52 +0530

    created a submission

--- script.js
+++
@@ -1,5 +0,0 @@
-describe('example to-do app', () => {
-  it('3 audios with heading are there', () => {
-    // your test code here
-  }, { timeout: 10000 }); // Increase timeout to 10000ms (10 seconds)
-});
